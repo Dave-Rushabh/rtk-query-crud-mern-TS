07ddc80d3b167dc8ab88132db638429770198f89
dave-rushabh <rushabh.dave24@gmail.com>
2023-01-21 23:51:47 +0530

    Prettier added

--- server/server.js
+++ server/server.js
@@ -1,31 +1,31 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import connectDB from "./db/config.js";
-import { router } from "./routes/route.js";
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import connectDB from './db/config.js'
+import { router } from './routes/route.js'
 
 // create express HTTP server with Node Js
-const app = express();
+const app = express()
 
 // adding middlewares for server to use cors, json & env variables
-app.use(express.json());
-app.use(cors());
-const envErrorsIdentifier = dotenv.config({ debug: process.env.DEBUG });
+app.use(express.json())
+app.use(cors())
+const envErrorsIdentifier = dotenv.config({ debug: process.env.DEBUG })
 
 if (envErrorsIdentifier.error) {
-  throw envErrorsIdentifier.error;
+  throw envErrorsIdentifier.error
 }
 
 // utilizing the port defined in the env file to target the server on this port
-const PORT = process.env.PORT_NUMBER;
+const PORT = process.env.PORT_NUMBER
 
 // Connect the express server with MONGODB
-connectDB();
+connectDB()
 
 // listen to the routes
-app.use("/api", router);
+app.use('/api', router)
 
 // listeing to the server on the given path
 app.listen(PORT || 5000, () => {
-  console.log(`server started on ${PORT}`);
-});
+  console.log(`server started on ${PORT}`)
+})
