edca6a2878a5f0ace3751a10825ebd64f359adb7
dave-rushabh <rushabh.dave24@gmail.com>
2023-01-21 20:05:28 +0530

    Backend setup completed

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./db/config.js";
+
+// create express HTTP server with Node Js
+const app = express();
+
+// adding middlewares for server to use cors, json & env variables
+app.use(express.json());
+app.use(cors());
+const envErrorsIdentifier = dotenv.config({ debug: process.env.DEBUG });
+
+if (envErrorsIdentifier.error) {
+  throw envErrorsIdentifier.error;
+}
+
+// utilizing the port defined in the env file to target the server on this port
+const PORT = process.env.PORT_NUMBER;
+
+// Connect the express server with MONGODB
+connectDB();
+
+// listeing to the server on the given path
+app.listen(PORT || 5000, () => {
+  console.log(`server started on ${PORT}`);
+});
